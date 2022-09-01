e8e6566bc0bd143700887cc2140074d3662619cd
mutuajames <njorogejames880@gmail.com>
2022-09-01 18:29:26 +0300

    Final part - Forwarding

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"Go-Projects/handler"
+	"Go-Projects/store"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -9,12 +11,24 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener !",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
 		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
 	}
+
 }
