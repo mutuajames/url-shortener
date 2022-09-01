20ab44485ef8d16e03a13048f8ac9ebfb6ecb4c2
mutuajames <njorogejames880@gmail.com>
2022-09-01 18:00:48 +0300

    Short Link Generator

---
+++ shortener/shorturl_generator.go
@@ -0,0 +1,34 @@
+package shortener
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/itchyny/base58-go"
+	"math/big"
+	"os"
+)
+
+//We will be using  SHA256 to hash the initial inputs during the process
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
+
+//This binary to text encoding will be used to provide the final output of the process. @base58
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
+
+func GenerateShortLink(initialLink string, userId string) string {
+	urlHashBytes := sha256Of(initialLink + userId)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:8]
+}
